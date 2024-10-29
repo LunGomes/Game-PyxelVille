7c212e2db7383409966bda491c9f985c1b153ff8
Laboratório FIAP <giovannalb070607@gmail.com>
2024-10-29 11:25:03 -0300

    animações arrumadinhas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,15 +11,18 @@ public class Player : MonoBehaviour {
 	float horizontalMove = 0f;
 	bool jump = false;
 	bool crouch = false;
+	public Animator animator;
 	
 	// Update is called once per frame
 	void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		animator.SetFloat("speed", Mathf.Abs(horizontalMove));
 
 		if (Input.GetButtonDown("Jump"))
 		{
 			jump = true;
+			animator.SetBool("Pular", true);
 		}
 
 		if (Input.GetButtonDown("Crouch"))
@@ -31,6 +34,11 @@ public class Player : MonoBehaviour {
 		}
 
 	}
+	
+		public void OnLanding()
+	{
+		animator.SetBool("Pular", false);
+	}
 
 	void FixedUpdate ()
 	{
@@ -38,4 +46,5 @@ public class Player : MonoBehaviour {
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
 		jump = false;
 	}
+
 }
